7935b51812d03b459412cd14e0a66de07077b648
Eduard Baumeister <eduard.baumeister1@gmail.com>
2024-05-28 16:21:31 +0200

    small changes

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'KKS Hambrücken 1931 e.V.';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
