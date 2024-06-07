e6712be46ab47573307b6fa534484107f02acdc5
Robin Walter <robin.walter555@hotmail.de>
2024-06-07 11:57:12 +0200

    added structure for PhotoGalleries; removed damn background gradient;

--- src/content/config.ts
+++ src/content/config.ts
@@ -19,11 +19,29 @@ const pages = defineCollection({
 	schema: z.object({
 		path: z.string(),
 		title: z.string(),
-		description: z.string(),
+		description: z.string().optional(),
 		// Transform string to Date object
-		pubDate: z.coerce.date(),
+		pubDate: z.coerce.date().optional(),
 		updatedDate: z.coerce.date().optional(),
 	}),
 });
 
-export const collections = { blog, pages };
+const photo_gallery = defineCollection({
+	type: 'content',
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		path: z.string(),
+		title: z.string(),
+		description: z.string().optional(),
+		thumbnail: z.string().optional(),
+		// Transform string to Date object
+		pubDate: z.coerce.date().optional(),
+		updatedDate: z.coerce.date().optional(),
+		photos: z.array(z.object({
+			title: z.string(),
+			photo: z.string()	
+		})),
+	}),
+});
+
+export const collections = { blog, pages, photo_gallery };
